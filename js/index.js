64e8403bdd72bce60197651c29dc712804a143a2
심초은 <chdms4906@naver.com>
2025-03-24 14:43:37 +0900

    포트폴리오 재업로드

---
+++ js/index.js
@@ -0,0 +1,66 @@
+$(function(){
+    // swipe
+    var swiper = new Swiper(".mySwiper", {
+        effect: 'fade',
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        on: {
+            slideChange: function () {
+                var swipeIdx = swiper.realIndex + 1;
+
+                console.log(swipeIdx)
+
+                if (swipeIdx == 2 || swipeIdx == 6 || swipeIdx == 7 || swipeIdx == 9) {
+                    $('.work .main-tit').css('text-shadow','0px 0px 20px rgba(0, 0, 0, 0.8)');
+                } else {
+                    $('.work .main-tit').css('text-shadow','0px 0px 20px rgba(255, 255, 255, 0.8)');
+                }
+            }
+        }
+    });
+
+    //scroll
+    $(window).on('scroll', function(e){
+        $('.img .bg-img, .img-wrap .bg-img').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'width':'0'},700);
+            }
+            
+        }); 
+    });
+
+    // modal
+    var posY;
+    $('.button.view').on('click', function(e){
+        var buttonId = $(this).attr('id');
+        posY = $(window).scrollTop();
+
+        $('#modal-container').removeAttr('class').addClass('on');
+        $('body').addClass('modal-active');
+        $("body").css("top", -posY);
+
+        // console.log(posY)
+        
+        $('#modal-container .modal-bg .modal').each(function (index, item) {
+            if($(this).attr('id') == buttonId) {
+                $('.modal').hide();
+                $(this).show();
+            }
+        });
+
+    });
+    
+    $('#modal-container').on('click', function(e){
+        if($(e.target).parents('.modal').length < 1){
+            $(this).addClass('out');
+            $('body').removeClass('modal-active');
+            posY = $(window).scrollTop(posY);
+        }
+    });
+})
